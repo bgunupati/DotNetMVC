b7c48855ffe940cc1c9bbd4834862ddaba682b55
Bharath Gunupati <bgunupati@gmail.com>
2018-06-05 20:53:06 -0400

    Added ActionResult and ViewBag example

---
+++ MVCBasics/MVCBasics/Controllers/CountriesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBasics.Controllers
+{
+    public class CountriesController : Controller
+    {
+        // GET: Countries
+        public ActionResult Index()
+        {
+            ViewBag.listCountries =  
+                new List<string>()
+                    {
+                        "India",
+                        "Australia",
+                        "Canada",
+                        "USA",
+                        "United Kingdom"
+                    };
+            return View();
+        }
+    }
+}
